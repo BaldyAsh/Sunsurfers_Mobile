539b783218a7b153fc2b3c37271678f6c15108ee
Anton Grigorev <antongrigorjev2010@gmail.com>
2018-12-26 01:18:19 +0300

    deleted to fix all

--- index.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import Sunsurfers from './src/app';
-
-AppRegistry.registerComponent('sunsurfers', () => Sunsurfers);
--- src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Button';
-export * from './Card';
-export * from './CardSection';
-export * from './Input';
-export * from './Spinner';
-export * from './Logo';
-export * from './Photo';
